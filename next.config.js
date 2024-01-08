48c55b3ed19b4d9c81fbe1fbc0d99737e4c7e099
mi-c0 <59290502+mic0ishere@users.noreply.github.com>
2024-01-08 22:58:51 +0100

    automatically parse config.yaml to env variables

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
+const { parse } = require("yaml");
+const fs = require("fs").promises;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["geist"],
 };
 
-module.exports = nextConfig;
+module.exports = async () => {
+  const configYaml = parse(await fs.readFile("./config.yaml", "utf8"));
+  const publicConfigYaml = Object.fromEntries(
+    Object.entries(configYaml).map(([key, value]) => [
+      key.startsWith("_")
+        ? key.toUpperCase().replace("_", "")
+        : "NEXT_PUBLIC_" + key.toUpperCase(),
+      value,
+    ])
+  );
+
+  return {
+    ...nextConfig,
+    env: {
+      ...nextConfig.env,
+      ...publicConfigYaml,
+    },
+  };
+};
