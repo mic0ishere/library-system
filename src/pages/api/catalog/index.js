59e7775b77b0f527bcd3402d3a74db21a0774f1e
mi-c0 <59290502+mic0ishere@users.noreply.github.com>
2024-02-05 22:52:45 +0100

    add api for adding books

--- src/pages/api/catalog/index.js
+++ src/pages/api/catalog/index.js
@@ -1,6 +1,9 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]";
 import prisma from "@/lib/prisma";
+import isAdmin from "@/lib/is-admin";
+import Joi from "joi/lib";
+import { bookSchema } from "@/lib/book-schema";
 
 export default async function handler(req, res) {
   const session = await getServerSession(req, res, authOptions);
@@ -8,7 +11,42 @@ export default async function handler(req, res) {
     res.status(401).end();
     return;
   }
-  
-  const books = await prisma.book.findMany();
-  res.status(200).json(books);
+
+  if (req.method === "GET") {
+    const books = await prisma.book.findMany();
+    res.status(200).json(books);
+  } else if (req.method === "POST" && isAdmin(session.user.email)) {
+    const data = req.body;
+
+    try {
+      Joi.assert(data, bookSchema);
+    } catch (error) {
+      res.status(400).json({
+        message:
+          "Invalid data provided. Please check information and try again.",
+        type: "error",
+      });
+      return console.error(error);
+    }
+
+    try {
+      await prisma.book.create({
+        data,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message:
+          "Error occured while adding book to the catalog. Please try again later.",
+        type: "error",
+      });
+      return console.error(error);
+    }
+
+    res.status(201).json({
+      message: `Added ${data.title} to the catalog`,
+      type: "success",
+    });
+  } else {
+    res.status(405).end();
+  }
 }
