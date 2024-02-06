0746d2c0efb7b417829b08c184c5dbf05194ea3d
mi-c0 <59290502+mic0ishere@users.noreply.github.com>
2024-02-06 23:08:39 +0100

    add overdue books to the returns page

--- src/pages/manage/returns.js
+++ src/pages/manage/returns.js
@@ -16,8 +16,11 @@ import isAdmin from "@/lib/is-admin";
 import prisma from "@/lib/prisma";
 import dateDifference from "@/lib/date-difference";
 
-export default function ManageReturns({ booksStr }) {
-  const [books, setBooks] = useState(JSON.parse(booksStr));
+export default function ManageReturns({ confirmationStr, overdueStr }) {
+  const [confirmationBooks, setConfirmationBooks] = useState(
+    JSON.parse(confirmationStr)
+  );
+  const overdueBooks = JSON.parse(overdueStr);
 
   async function confirmBook(id) {
     try {
@@ -34,7 +37,7 @@ export default function ManageReturns({ booksStr }) {
       ).json();
 
       if (response.type === "success") {
-        setBooks(books.filter((b) => b.id !== id));
+        setConfirmationBooks(confirmationBooks.filter((b) => b.id !== id));
         return response;
       } else {
         throw new Error(response.message);
@@ -47,23 +50,23 @@ export default function ManageReturns({ booksStr }) {
 
   return (
     <div className="w-full pt-8 pb-16 max-w-[900px]">
-      <h1 className="text-4xl">Manage returns</h1>
+      <h1 className="text-4xl">Manage book statuses</h1>
       <p className="mt-2 mb-4">
         Here, you can see the books that were marked as returned by the users
         and are waiting for your confirmation on their status in order to be
         available in the catalog again.
       </p>
-      {books.length === 0 && (
-        <div className="text-center">
-          <h1 className="text-2xl font-semibold mt-24">Congratulations!</h1>
+      {confirmationBooks.length === 0 && (
+        <div className="text-center py-12">
+          <h1 className="text-2xl font-semibold">Congratulations!</h1>
           <p className="text-lg">
             There are no books waiting for confirmation at the moment.
           </p>
         </div>
       )}
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 w-full mt-12">
-        {books.length > 0 &&
-          books.map((book) => (
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 w-full mt-4">
+        {confirmationBooks.length > 0 &&
+          confirmationBooks.map((book) => (
             <Card
               key={book.id}
               className="h-full flex flex-col justify-between"
@@ -108,7 +111,11 @@ export default function ManageReturns({ booksStr }) {
                 >
                   Confirm return
                 </Button>
-                <Link href={`/manage/users/${book.rentedBy.userId}`}>
+                <Link
+                  href={`/manage/users/${book.rentedBy.userId}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <Button className="w-full mt-2" variant="outline" size="sm">
                     View user <ExternalLinkIcon className="ml-1 h-4 w-4" />
                   </Button>
@@ -117,6 +124,59 @@ export default function ManageReturns({ booksStr }) {
             </Card>
           ))}
       </div>
+      {overdueBooks.length > 0 && (
+        <>
+          <h2 className="text-2xl mt-8">Overdue books</h2>
+          <p className="mt-2 mb-4">
+            Here, you can see the books that are currently rented and are
+            overdue.
+          </p>
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 w-full mt-4">
+            {overdueBooks
+              .sort((a, b) => dateDifference(a.dueDate, b.dueDate))
+              .map((book) => (
+                <Card
+                  key={book.id}
+                  className="h-full flex flex-col justify-between"
+                >
+                  <CardHeader className="pb-3">
+                    <CardTitle className="text-xl">{book.title}</CardTitle>
+                    <CardDescription>
+                      <span className="font-semibold">Rented by:</span>{" "}
+                      {book.rentedBy.name}
+                    </CardDescription>
+                    <ul className="text-sm ml-4 md:list-disc">
+                      <li>
+                        Rented: {new Date(book.rentedAt).toLocaleDateString()}
+                      </li>
+                      <li>
+                        Due: {new Date(book.dueDate).toLocaleDateString()} (
+                        <span className="font-semibold">
+                          {new Intl.RelativeTimeFormat("en").format(
+                            dateDifference(book.dueDate, Date.now()),
+                            "day"
+                          )}
+                        </span>
+                        )
+                      </li>
+                    </ul>
+                  </CardHeader>
+                  <CardContent>
+                    <Link
+                      href={`/manage/users/${book.rentedBy.userId}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="w-full" variant="outline" size="sm">
+                        View user <ExternalLinkIcon className="ml-1 h-4 w-4" />
+                      </Button>
+                    </Link>
+                  </CardContent>
+                </Card>
+              ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
@@ -138,10 +198,13 @@ export async function getServerSideProps(context) {
 
   const books = await prisma.book.findMany({
     where: {
-      status: "BACK_SOON",
+      status: {
+        in: ["BACK_SOON", "RENTED"],
+      },
     },
     select: {
       id: true,
+      status: true,
       title: true,
       rentedAt: true,
       dueDate: true,
@@ -157,7 +220,15 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
-      booksStr: JSON.stringify(books || []),
+      confirmationStr: JSON.stringify(
+        books.filter((x) => x.status === "BACK_SOON") || []
+      ),
+      overdueStr: JSON.stringify(
+        books.filter(
+          (x) =>
+            x.status === "RENTED" && dateDifference(x.dueDate, Date.now()) < 0
+        ) || []
+      ),
     },
   };
 }
