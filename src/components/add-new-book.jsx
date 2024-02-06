b71133eabe60b2ce6c39cc07a90d070db66d53d8
mi-c0 <59290502+mic0ishere@users.noreply.github.com>
2024-02-06 10:41:10 +0100

    extract BookForm

--- src/components/add-new-book.jsx
+++ src/components/add-new-book.jsx
@@ -14,15 +14,6 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 import { useSWRConfig } from "swr";
@@ -30,13 +21,27 @@ import { useState } from "react";
 import { useMediaQuery } from "@/lib/use-media-query";
 import { toast } from "sonner";
 
-import { useForm } from "react-hook-form";
-import { joiResolver } from "@hookform/resolvers/joi";
-import { bookSchema } from "@/lib/book-schema";
+import BookForm from "./book-form";
 
 function AddNewBookModal({ children }) {
+  const { mutate } = useSWRConfig();
   const [open, setOpen] = useState(false);
   const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  const onSubmit = async (data) => {
+    setOpen(false);
+
+    const afterPromise = (data) => {
+      mutate("/api/catalog");
+      return data.message;
+    };
+
+    toast.promise(addNewBook(data), {
+      loading: "Adding new book...",
+      success: afterPromise,
+      error: afterPromise,
+    });
+  };
 
   if (isDesktop)
     return (
@@ -46,7 +51,7 @@ function AddNewBookModal({ children }) {
           <DialogHeader>
             <DialogTitle>New book</DialogTitle>
           </DialogHeader>
-          <NewBookForm open={setOpen} />
+          <BookForm buttonLabel="Add to catalog" onSubmit={onSubmit} />
         </DialogContent>
       </Dialog>
     );
@@ -57,7 +62,7 @@ function AddNewBookModal({ children }) {
       <DrawerContent>
         <DrawerHeader>
           <DrawerTitle>New book</DrawerTitle>
-          <NewBookForm open={setOpen} />
+          <BookForm buttonLabel="Add to catalog" onSubmit={onSubmit} />
         </DrawerHeader>
         <DrawerFooter className="pt-2">
           <DrawerClose asChild>
@@ -69,86 +74,9 @@ function AddNewBookModal({ children }) {
   );
 }
 
-function NewBookForm({ open }) {
-  const { mutate } = useSWRConfig();
-
-  const form = useForm({
-    resolver: joiResolver(bookSchema),
-    defaultValues: {
-      title: "",
-      author: "",
-      year: "",
-      pages: "",
-      isbn: "",
-    },
-  });
-
-  return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(async (data) => {
-          open(false);
-
-          const afterPromise = (data) => {
-            mutate("/api/catalog");
-            return data.message;
-          };
-
-          toast.promise(addNewBook(data), {
-            loading: "Adding new book...",
-            success: afterPromise,
-            error: afterPromise,
-          });
-        })}
-      >
-        <div className="flex flex-col gap-2">
-          <Field form={form} name="title">
-            Title
-          </Field>
-          <Field form={form} name="author">
-            Author
-          </Field>
-          <div className="w-full flex flex-row sm:space-x-2">
-            <Field form={form} name="year">
-              Publishing year
-            </Field>
-            <Field form={form} name="pages" type="number">
-              Pages count
-            </Field>
-          </div>
-          <Field form={form} name="isbn">
-            ISBN
-          </Field>
-        </div>
-        <Button className="w-full mt-4" type="submit">
-          Add to catalog
-        </Button>
-      </form>
-    </Form>
-  );
-}
-
-function Field({ form, name, children, ...props }) {
-  return (
-    <FormField
-      control={form.control}
-      name={name}
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>{children}</FormLabel>
-          <FormControl>
-            <Input {...field}  />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-  );
-}
-
 async function addNewBook(data) {
   try {
-    console.log(data)
+    console.log(data);
 
     const response = await (
       await fetch(`/api/catalog`, {
---
+++ src/components/book-form.jsx
@@ -0,0 +1,81 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { useForm } from "react-hook-form";
+import { joiResolver } from "@hookform/resolvers/joi";
+import { bookSchema } from "@/lib/book-schema";
+
+import { Button } from "@/components/ui/button";
+
+function BookForm({
+  buttonLabel,
+  onSubmit,
+  defaultValues = {
+    title: "",
+    author: "",
+    year: "",
+    pages: "",
+    isbn: "",
+  },
+}) {
+  const form = useForm({
+    resolver: joiResolver(bookSchema),
+    defaultValues,
+  });
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex flex-col gap-2">
+          <Field form={form} name="title">
+            Title
+          </Field>
+          <Field form={form} name="author">
+            Author
+          </Field>
+          <div className="w-full flex flex-row sm:space-x-2">
+            <Field form={form} name="year">
+              Publishing year
+            </Field>
+            <Field form={form} name="pages" type="number">
+              Pages count
+            </Field>
+          </div>
+          <Field form={form} name="isbn">
+            ISBN
+          </Field>
+        </div>
+        <Button className="w-full mt-4" type="submit">
+          {buttonLabel}
+        </Button>
+      </form>
+    </Form>
+  );
+}
+
+function Field({ form, name, children, type }) {
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{children}</FormLabel>
+          <FormControl>
+            <Input {...field} type={type} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
+
+export default BookForm;
